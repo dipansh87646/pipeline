90373aad410b2b8b3ff8858d032165dcd5999b45
Dipansh Tailor <dipanshtailor10@gmail.com>
2024-12-03 11:25:52 +0530

    added

---
+++ testing/test.py
@@ -0,0 +1,10 @@
+from operation.main import add , sub
+def test_add() -> None:
+    assert add(2,3) == 5
+    assert add(-1,1) == 0
+
+def test_sub() -> None:
+    assert sub(7,3) == 4
+    assert sub(4,3) == 1
+    assert sub(5,2) == 3
+    assert sub(10,5) == 5    
